a3a7d876441046fc46598b54190189ba02a034c5
Julian Lopez <a20490712@itmexicali.edu.mx>
2023-03-11 11:08:51 -0800

    Added promo view at start

--- project/reactfirst/src/pages/payment.jsx
+++ project/reactfirst/src/pages/payment.jsx
@@ -1,10 +1,11 @@
-
+import { Link } from "react-router-dom";
 const Payment = () => {
     
     return (
-        <div>
+        <div className="payment">
             <h1 className="title">Order created</h1>
             <p>You have made a new order, wait for the driver to pick it up and deliver it to you.</p>
+            <Link className="inline-btn" aria-current="page" to="/home">Go back home</Link>
         </div>
     )
 }
